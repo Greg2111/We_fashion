5aeb8d071aa20d98ef6dc9943357115064c3ef7e
Grégory Ehui <misterg40@gmail.com>
2023-02-06 23:41:59 +0000

    l'envoie final je pense

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -21,7 +21,7 @@ class ProductFactory extends Factory
             "description"=>$this->faker->streetName(),
             "price"=>$this->faker->randomFloat([20,700]),
             "size"=>$this->faker->randomElement(['XS','S', 'M', 'L', 'XL']),
-            'image'=>$this->faker->file('C:\Users\miste\OneDrive\Bureau\php2k23\We_Fashion_ressources\images\hommes', 'storage/app/public/images/hommes', false),
+            'image'=>$this->faker->file('C:\Users\miste\OneDrive\Bureau\php2k23\We_Fashion_ressources\images\hommes', 'storage/app/public/images/hommes', true),
             "visibility"=>$this->faker->randomElement(['publié', 'non publié']),
             "status"=>$this->faker->randomElement(['standard', 'en solde']),
             "reference"=>$this->faker->imageUrl(640,480),
